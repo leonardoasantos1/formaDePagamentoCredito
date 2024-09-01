d80517c974cb2b6d7d2bf718c385495aa660a881
leonardoasantos1 <leonardoasantos1@outlook.com>
2024-09-01 20:28:04 -0300

    estilização e validação

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,24 +16,53 @@ names.addEventListener('input', () => {
 
 numbers.addEventListener('input', () => {
     textNumber.textContent = numbers.value;
+    let numberLength = numbers.value.length;
 
     if(numbers.value.charAt(0) === '4') {
-        srcBandeira.innerHTML = '<img src="assets/images/logo-visa.png" alt="logo-visa">';
-    } else if(numbers.value.charAt(0) === '5'){
-        srcBandeira.innerHTML = '<img src="assets/images/logo-master.png" alt="logo-master">';
-    } else if(numbers.value.charAt(0) === '6'){
-        srcBandeira.innerHTML = '<img src="assets/images/logo-elo.png" alt="logo-el">';
+        srcBandeira.innerHTML = '<img src="assets/images/logo-visa.png" alt="logo-visa" width="60">';
+    } else if(numbers.value.charAt(0) === '5') {
+        srcBandeira.innerHTML = '<img src="assets/images/logo-master.png" alt="logo-master" width="60">';
+    } else if(numbers.value.charAt(0) === '6') {
+        srcBandeira.innerHTML = '<img src="assets/images/logo-elo.png" alt="logo-el" width="60">';
     } else {
         srcBandeira.innerHTML = '';
+    }
+
+    if(numberLength === 4 || numberLength === 9 || numberLength === 14) {
+        numbers.value += ' ';
+    }
+
+    if(numberLength === 19) {
+        numbers.style.border = '1px solid green'
+    } else {
+        numbers.style.border = '1px solid red'
     }
 });
 
 datas.addEventListener('input', () => {
     textData.textContent = datas.value;
+    let dataLength = datas.value.length;
+
+    if(dataLength === 2) {
+        datas.value += "/";
+    }
+
+    if(dataLength === 5) {
+        datas.style.border = '1px solid green'
+    } else {
+        datas.style.border = '1px solid red'
+    }
 });
 
 cvv.addEventListener('input', () => {
     textCvv.textContent = cvv.value;
+    let cvvLength = cvv.value.length;
+
+    if(cvvLength === 3) {
+        cvv.style.border = '1px solid green'
+    } else if (!NaN){
+        cvv.style.border = '1px solid red'
+    }
 });
 
 
